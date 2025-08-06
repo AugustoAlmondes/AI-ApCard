babe302bac27714977076fd95eb1b91d90f55415
AugustoAlmondes <augusto7666@gmail.com>
2025-08-06 10:43:31 -0300

    feat: create a new component (Chat.tsx)

---
+++ src/pages/Chat/Chat.tsx
@@ -0,0 +1,10 @@
+// import './Chat.module.css';
+// import styles from './Chat.module.css';
+
+export default function Chat() {
+    return (
+        <>
+            <h1>Chat</h1>
+        </>
+    );
+}
