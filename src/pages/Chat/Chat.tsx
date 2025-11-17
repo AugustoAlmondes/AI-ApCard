83f288a8173c0333ed407b72741de3192b30023a
AugustoAlmondes <augusto7666@gmail.com>
2025-11-17 15:09:05 -0300

    feat: implement chat functionality with context provider and API integration

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -3,12 +3,15 @@ import BotBalloon from "../../components/BotBalloon";
 import UserBalloon from "../../components/UserBalloon";
 import { BsArrowLeft } from "react-icons/bs";
 import { useNavigate } from "react-router";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useChat } from "../../hooks/useChat";
+import { ThreeDot } from "react-loading-indicators";
+import Background from "/src/assets/background.mp4";
 
 export default function Chat() {
     const navigate = useNavigate();
-    const { messages, sendMessage } = useChat();
+    const { messages, sendMessage, disableButton } = useChat();
+    const chatContainerRef = useRef<HTMLDivElement>(null);
     const [inputValue, setInputValue] = useState('');
 
     const handleSubmit = (e: React.FormEvent) => {
@@ -19,8 +22,20 @@ export default function Chat() {
         }
     };
 
+    useEffect(() => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTo({
+                top: chatContainerRef.current.scrollHeight,
+                behavior: 'smooth'
+            });
+        }
+    }, [messages, disableButton]);
+
     return (
-        <div className="relative flex flex-col items-center py-7 h-screen bg-gradient-to-b from-[#021728] to-[#001323]">
+        <div className="relative flex flex-col items-center py-7 h-screen">
+            <a className="text-white/10 z-10 absolute bottom-0 text-sm font-extralight" href="https://www.vecteezy.com/free-videos/abstract" target="_blank">Abstract Stock Videos by Vecteezy</a>
+            <video src={Background} autoPlay loop muted className="h-[90%] w-full object-cover absolute filter brightness-60 z-[-1]"></video>
+
             <div className={`absolute top-0 left-0 w-full flex justify-center bg-gradient-to-b via-[#001323] from-[#021728] to-transparent py-10 z-20`} />
 
             <div className={`absolute top-0 left-5 flex items-center gap-2 justify-center py-4 z-20 text-white text-md opacity-50 hover:opacity-100 cursor-pointer`}
@@ -31,14 +46,17 @@ export default function Chat() {
 
             <h3 className={`absolute top-0 left-1/2 transform -translate-x-1/2 flex justify-center py-4 z-20 text-white text-md opacity-50`}>Ap Robot</h3>
 
-            <div className="flex flex-col w-full max-h-[90vh] gap-10 max-w-4xl h-full px-4 pt-13 pb-32 overflow-y-auto space-y-4 scrollbar-hide">
+            <div
+                ref={chatContainerRef}
+                className="flex flex-col w-full max-h-[90vh] gap-10 max-w-4xl h-full px-4 pt-13 pb-32 overflow-y-auto space-y-4 scrollbar-hide"
+            >
                 {messages.map((message) => (
                     <div key={message.id}>
                         {message.sender === 'bot' ? (
                             <BotBalloon
                                 text={message.text}
                                 time={message.time}
-                                typing={message.isTyping || false}
+                                typing={message.isTyping || true}
                             />
                         ) : (
                             <UserBalloon
@@ -53,7 +71,7 @@ export default function Chat() {
             <div className="absolute bottom-0 left-0 w-full flex justify-center bg-gradient-to-t from-[#001323] to-transparent py-6">
                 <form
                     onSubmit={handleSubmit}
-                    className="flex w-full max-w-2xl gap-2 px-4">
+                    className="grid grid-cols-[2fr_0.5fr] w-full max-w-2xl gap-2 px-4">
                     <input
                         type="text"
                         value={inputValue}
@@ -61,12 +79,20 @@ export default function Chat() {
                         placeholder="Digite sua mensagem..."
                         className="flex-1 bg-[#0a223a] text-white rounded-lg px-4 py-3 outline-none border border-[#1a2e47] focus:border-[#0984E9] transition relative z-[2]"
                     />
-                    <button
-                        type="submit"
-                        className="flex items-center gap-2 bg-[#0984E9] text-[white] px-5 py-3 rounded-lg hover:bg-[#0984E9] transition relative z-[2]"
-                    >
-                        Enviar <BiSend size={20} />
-                    </button>
+                    {
+                        disableButton ? (
+                            <div className="flex items-center justify-center text-[white] px-5 py-3 rounded-lg relative z-[2]">
+                                <ThreeDot color="#fff" size="small" />
+                            </div>
+                        ) : (
+                            <button
+                                type="submit"
+                                className="flex items-center gap-2 bg-[#0984E9] text-[white] px-5 py-3 rounded-lg hover:bg-[#0984E9] transition relative z-[2]"
+                            >
+                                Enviar <BiSend size={20} />
+                            </button>
+                        )
+                    }
                 </form>
                 <div className={`absolute bottom-0 left-0 w-full flex justify-center bg-gradient-to-t via-[#001323] from-[#021728] to-transparent py-23 z-[0]`} />
             </div>
